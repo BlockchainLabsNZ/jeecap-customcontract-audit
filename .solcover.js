6bf8f69abac43a38d30a51413a2a4275c541b0e7
Bruce Li <ryu9827@gmail.com>
2018-09-21 16:40:34 +1200

    add coverage setting

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,12 @@
 module.exports = {
-    skipFiles: ['Migrations.sol']
+    norpc:true,
+    skipFiles: ['Migrations.sol',
+                'token/BasicToken.sol',
+                'token/ERC20.sol',
+                'token/ERC20Basic.sol',
+                'token/SafeMath.sol',
+                'token/TestToken.sol',
+                'Crowdsale/Crowdsale.sol',
+                'Crowdsale/SafeERC20.sol'
+                ]
 };
